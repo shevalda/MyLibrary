62663d97ecfc79c2c4bb2444d9d2edf8e9d46ca5
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2021-02-14 12:43:56 +0700

    refactor library_spec.rb to make library a let

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -2,6 +2,7 @@ require './app/models/library'
 
 RSpec.describe Library do
   let(:book) { double }
+  let(:library) { described_class.new(2, 1, 3) }
 
   context '#initialize' do
     it 'raises an error is number of row is invalid' do
@@ -24,7 +25,6 @@ RSpec.describe Library do
 
     context 'with input 2 bookshelves of 1 row and 3 columns' do
       it 'returns Library with 2 bookshelves of 1 row and 3 columns' do
-        library = described_class.new(2, 1, 3)
         expect(library.bookshelves.length).to eq(2)
         expect(library.bookshelves[0].rows_size).to eq(1)
         expect(library.bookshelves[0].columns_size).to eq(3)
@@ -35,19 +35,15 @@ RSpec.describe Library do
           Shelf 1 with 1 rows and 3 columns is added
           Shelf 2 with 1 rows and 3 columns is added
         TEXT
-        expect { described_class.new(2, 1, 3) }.to output(expected_output).to_stdout
+        expect { library }.to output(expected_output).to_stdout
       end
     end
   end
 
   context '#put_book' do
-    before do
-      @library = described_class.new(2, 1, 3)
-    end
-
     context 'with empty bookshelves' do
       it 'returns book location on the first avaiable space on first shelf' do
-        result = @library.put_book(book)
+        result = library.put_book(book)
         expect(result).to eq({ shelf: 1, row: 1, column: 1 })
       end
     end
@@ -55,13 +51,14 @@ RSpec.describe Library do
     context 'with some unavailable spaces on the first bookshelf' do
       it 'returns book location of the first available space' do
         allow_any_instance_of(Bookshelf).to receive(:put_book).and_return({ row: 2, column: 2 })
-        result = @library.put_book(book)
+        result = library.put_book(book)
         expect(result).to eq({ shelf: 1, row: 2, column: 2 })
       end
     end
 
     context 'when some bookshelves already full' do
       before do
+        @library = described_class.new(2, 1, 3)
         3.times do
           @library.put_book(book)
         end
@@ -76,20 +73,16 @@ RSpec.describe Library do
     context 'when all bookshelves are full' do
       it 'returns false' do
         allow_any_instance_of(Bookshelf).to receive(:put_book).and_return(false)
-        result = @library.put_book(book)
+        result = library.put_book(book)
         expect(result).to eq(false)
       end
     end
   end
 
   context '#take_book_from' do
-    before do
-      @library = described_class.new(2, 1, 3)
-    end
-
     it 'returns Book when the selected positon has a book' do
       allow_any_instance_of(Bookshelf).to receive(:take_book_from).and_return(book)
-      result = @library.take_book_from(1, 1, 1)
+      result = library.take_book_from(1, 1, 1)
       expect(result).to eq(book)
     end
   end
