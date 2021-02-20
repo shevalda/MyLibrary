442896e25d4b7e332970fc69c8adb83596341bc7
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2021-02-20 21:36:07 +0700

    remove puts in Library initialize

--- app/models/library.rb
+++ app/models/library.rb
@@ -7,9 +7,6 @@ class Library
     raise 'Invalid number of bookhelves' unless bookshelves_count.positive?
 
     @bookshelves = Array.new(bookshelves_count) { Bookshelf.new(row, column) }
-    @bookshelves.each.with_index(1) do |bookshelf, index|
-      puts "Shelf #{index} with #{bookshelf.rows_size} rows and #{bookshelf.columns_size} columns is added"
-    end
   end
 
   def put_book(book)
--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -29,14 +29,6 @@ RSpec.describe Library do
         expect(library.bookshelves[0].rows_size).to eq(1)
         expect(library.bookshelves[0].columns_size).to eq(3)
       end
-
-      it "prints 'Shelf N with 1 rows and 3 columns is added' twice" do
-        expected_output = <<~TEXT
-          Shelf 1 with 1 rows and 3 columns is added
-          Shelf 2 with 1 rows and 3 columns is added
-        TEXT
-        expect { library }.to output(expected_output).to_stdout
-      end
     end
   end
 
