ca10eb56171282e4aa3549b03e1fc6e6d29dc754
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2021-02-27 18:28:56 +0700

    extract book_list_output to LibraryCommand

--- app/commands/library_command.rb
+++ app/commands/library_command.rb
@@ -5,7 +5,7 @@ class LibraryCommand
     @library = Library.instance
   end
 
-  def execute(arguments)
+  def execute(_arguments)
     raise NotImplementedError, 'execute not defined'
   end
 
@@ -14,6 +14,21 @@ class LibraryCommand
   def check_command_length(input, expected_count)
     arguements_length = input.split('|').length
     raise "Expected #{expected_count} arguments, given #{arguements_length}" unless arguements_length == expected_count
+  end
+
+  def book_list_output(book_list)
+    output = []
+    book_list.each do |shelf_info|
+      shelf_info[:list_books].each do |book_info|
+        output << <<~TEXT.split.join(' ')
+          #{position_string(shelf_info[:shelf], book_info[:row], book_info[:column])}:
+          #{book_info[:book].isbn} |
+          #{book_info[:book].title} |
+          #{book_info[:book].author}
+        TEXT
+      end
+    end
+    output.join("\n")
   end
 
   def position_string(bookshelf_pos, row_pos, column_pos)
--- app/commands/list_books_command.rb
+++ app/commands/list_books_command.rb
@@ -12,17 +12,6 @@ class ListBooksCommand < LibraryCommand
   def output_command(book_list)
     return 'Library is empty' if book_list.empty?
 
-    output = []
-    book_list.each do |shelf_info|
-      shelf_info[:list_books].each do |book_info|
-        output << <<~TEXT.split.join(' ')
-          #{position_string(shelf_info[:shelf], book_info[:row], book_info[:column])}:
-          #{book_info[:book].isbn} |
-          #{book_info[:book].title} |
-          #{book_info[:book].author}
-        TEXT
-      end
-    end
-    output.join("\n")
+    book_list_output(book_list)
   end
 end
