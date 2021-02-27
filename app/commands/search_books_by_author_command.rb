95b823b39bc72211e61b7decdf6711fe83bb57e1
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2021-02-27 20:52:09 +0700

    add condition when some book matches keyword in SearchBookByAuthorCommand

--- app/commands/search_books_by_author_command.rb
+++ app/commands/search_books_by_author_command.rb
@@ -4,6 +4,22 @@ class SearchBooksByAuthorCommand < LibraryCommand
   def execute(arguments)
     check_command_length(arguments, 2)
 
-    'Book not found!'
+    keyword = parse_command(arguments)
+    book_list = @library.search_books_by_author(keyword)
+
+    output_command(book_list)
+  end
+
+  private
+
+  def parse_command(arguments)
+    _, keyword = arguments.split('|')
+    keyword
+  end
+
+  def output_command(book_list)
+    return 'Book not found!' if book_list.empty?
+
+    book_list_output(book_list)
   end
 end
--- spec/commands/search_books_by_author_command_rspec.rb
+++ spec/commands/search_books_by_author_command_rspec.rb
@@ -2,6 +2,8 @@ require './app/commands/search_books_by_author_command'
 require './app/models/library'
 
 RSpec.describe SearchBooksByAuthorCommand do
+  let(:book) { double(title: 'Title', author: 'Author', isbn: '1234567890') }
+
   before do
     Library.instance.build(2, 1, 3)
     @command = described_class.new
@@ -17,6 +19,21 @@ RSpec.describe SearchBooksByAuthorCommand do
     allow_any_instance_of(Library).to receive(:search_books_by_author).and_return([])
     input = 'search_books_by_author|penulis'
     expected_output = 'Book not found!'
+    expect(@command.execute(input)).to eq(expected_output)
+  end
+
+  it 'returns all books that match the keyword' do
+    book_list = [
+      { shelf: 1, list_books: [{ row: 1, column: 1, book: book }] },
+      { shelf: 2, list_books: [{ row: 1, column: 1, book: book }] }
+    ]
+    allow_any_instance_of(Library).to receive(:search_books_by_author).and_return(book_list)
+
+    expected_output = <<~TEXT.chomp
+      010101: 1234567890 | Title | Author
+      020101: 1234567890 | Title | Author
+    TEXT
+    input = 'search_books_by_author|author'
     expect(@command.execute(input)).to eq(expected_output)
   end
 end
