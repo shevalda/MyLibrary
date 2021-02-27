c0426f93cf7ea445e291317a234be392df86d460
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2021-02-27 18:16:04 +0700

    add condition when no book match keyword in SearchBookByTitleCommand

--- app/commands/search_books_by_title_command.rb
+++ app/commands/search_books_by_title_command.rb
@@ -3,5 +3,7 @@ require './app/commands/library_command'
 class SearchBookByTitleCommand < LibraryCommand
   def execute(arguments)
     check_command_length(arguments, 2)
+
+    'Book not found!'
   end
 end
--- spec/commands/search_books_by_title_command_rspec.rb
+++ spec/commands/search_books_by_title_command_rspec.rb
@@ -14,4 +14,11 @@ RSpec.describe FindBookCommand do
     expected_output = 'Expected 2 arguments, given 1'
     expect { @command.execute(input) }.to raise_error(expected_output)
   end
+
+  it 'returns warning if no book matches the keyword' do
+    allow_any_instance_of(Library).to receive(:search_books_by_title).and_return([])
+    input = 'search_books_by_title|judul'
+    expected_output = 'Book not found!'
+    expect(@command.execute(input)).to eq(expected_output)
+  end
 end
